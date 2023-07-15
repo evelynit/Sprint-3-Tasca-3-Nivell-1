0f913146d52fa0dcc1ae5348c1a5f6a4b098efc1
evelynit <nyit001122@gmail.com>
2023-07-16 01:32:21 +0200

    Se ha añadido un throw new IllegalArgument

--- src/Product.java
+++ src/Product.java
@@ -2,6 +2,9 @@ public abstract class Product {
     protected double price;
 
     public Product(double price) {
+        if(price < 0) {
+            throw new IllegalArgumentException("El precio no puede ser negativo.");
+        }
         this.price = price;
     }
 
